747f86d29c4ddba550b3a1049bcc4f93daded195
Kostas Bartsokas <kosbarts@yahoo.com>
2021-09-13 13:52:16 +0300

    Fixed the Show All Kerning Pairs script
    I think that now it finds the first glyph in a group (if the group name is not a glyph name).

--- Spacing/Show All Kerning Pairs.py
+++ Spacing/Show All Kerning Pairs.py
@@ -1,63 +1,46 @@
-#MenuTitle: Show All Kerning Pairs
+#MenuTitle: Show All Kerning Pairσ
 # -*- coding: utf-8 -*-
 __doc__="""
 Show All Kerning Pairs for this Master in a new tab
 """
 import GlyphsApp
-# this script is flawed because its not comparing when it makes a GLYPH GLYPH whether the kerning pair has an exception, so it should get the kerning pair first, then check whether the @MMK GLYPH, or GLYPH @MMK, or @MMK @MMK is the same as GLYPH GLYPH, if not then go to next glyph in the group, otherwise in the end just go with the glyph (cus then there's no other instance where the @MMK kerning would apply)
 
-thisFont = Glyphs.font
+Font = Glyphs.font
 Doc = Glyphs.currentDocument
-selectedLayers = thisFont.selectedLayers
-selectedMaster = thisFont.selectedFontMaster
+selectedLayers = Font.selectedLayers
+selectedMaster = Font.selectedFontMaster
 masterID = selectedMaster.id
 
 editString = u""""""
 
-kernDict = thisFont.kerningDict()
-leftGroups = {}
-rightGroups = {}
-for g in thisFont.glyphs:
-	if g.rightKerningGroup:
-		group_name = g.rightKerningGroupId()
-		try:
-			leftGroups[group_name].append(g.name)
-		except:
-			leftGroups[group_name] = [g.name]
+leftGroupDefaults = {}
+rightGroupDefaults = {}
+for thisGlyph in Font.glyphs[::-1]:
+	if thisGlyph.leftKerningGroup:
+		leftGroupDefaults[thisGlyph.leftKerningGroup] = thisGlyph.name
+	if thisGlyph.rightKerningGroup:
+		rightGroupDefaults[thisGlyph.rightKerningGroup] = thisGlyph.name
 
-	if g.leftKerningGroup:
-		group_name = g.leftKerningGroupId()
-		try:
-			rightGroups[group_name].append(g.name)
-		except:
-			rightGroups[group_name] = [g.name]
-
-def nameMaker(kernGlyphOrGroup, side):
-	# if this is a kerning group
-	if kernGlyphOrGroup[0] == "@":
-		# right glyph, left kerning group
-		if side == "right":
-			try:
-				return rightGroups[kernGlyphOrGroup][0]
-			except:
-				pass
-		elif side == "left":
-			# left glyph, right kerning group
-			try:
-				return leftGroups[kernGlyphOrGroup][0]
-			except:
-				pass
+def nameMakerL(kernGlyph):
+	if kernGlyph[0] == "@":
+		Glyphname1 = rightGroupDefaults[kernGlyph[7:]]
+		return Glyphname1
 	else:
-		return thisFont.glyphForId_(kernGlyphOrGroup).name
+		return Font.glyphForId_(kernGlyph).name	
+        
+def nameMakerR(kernGlyph):
+	if kernGlyph[0] == "@":
+		Glyphname2 = leftGroupDefaults[kernGlyph[7:]]
+		return Glyphname2
+	else:
+		return Font.glyphForId_(kernGlyph).name	
 
-for L in thisFont.kerning[ masterID ].keys():
+for L in Font.kerning[ masterID ]:
 	try:
-		for R in thisFont.kerning[masterID][L].keys():
-			kernPair = "/%s/%s/space" % (nameMaker(L, "left"), nameMaker(R, "right")) 
+		for R in Font.kerning[masterID][L]:
+			kernPair = "/%s/%s  " % (nameMakerL(L), nameMakerR(R))
 			editString += kernPair
 	except:
 		pass
 
-# print editString
-
-thisFont.newTab(editString)
+Glyphs.font.newTab(editString)
